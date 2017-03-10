b25b7dce59060ce9c5de0911964e2b2db1f3fc0d
Philip Crotwell <crotwell@seis.sc.edu>
2017-03-10 13:49:00 -0500

    clean up example

--- example/test.js
+++ example/test.js
@@ -2,19 +2,17 @@
 
 // this comes from the seisplotjs bundle
 var traveltime = seisplotjs_traveltime;
-var wp = seisplotjs_waveformplot;
-var daysAgo = 1;
 
 var query = new traveltime.TraveltimeQuery()
   .evdepth(50)
   .distdeg(30)
   .phases("P,S");
-wp.d3.select("div.url")
+d3.select("div.url")
     .append("p")
     .text("URL: "+query.formURL());
 query.query().then(function(times) {
 console.log(" times :"+times.model+" "+times.arrivals.length);
-  wp.d3.select("div.traveltimes")
+  d3.select("div.traveltimes")
     .selectAll("p")
     .data(times.arrivals)
     .enter() 
@@ -28,7 +26,7 @@ console.log(" times :"+times.model+" "+times.arrivals.length);
     });
     
 }, function(reason) {
-wp.d3.select("div.traveltimes")
+d3.select("div.traveltimes")
     .append("p")
     .text("Reject: "+reason);
 });
